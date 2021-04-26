02867a4b2fe2c4c3e6b640186b633017cb19e142
Yulialeet <yuliaveselova1337@gmail.com>
2021-04-26 14:17:36 +0300

    Commit

--- src/components/ManagerComponents/DishesEdit/AddDish.js
+++ src/components/ManagerComponents/DishesEdit/AddDish.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { ActivityIndicator, Text, TextInput, View } from 'react-native'
-import DropDownPicker from 'react-native-dropdown-picker';
+import { ActivityIndicator, Alert, Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import DropDownPicker from 'react-native-dropdown-picker'
 import { connect } from 'react-redux'
 import myURL from '../../../CommonURL/myURL'
-import { AddDishStyle } from './AddDishStyle';
+import { AddDishStyle } from './AddDishStyle'
+import {launchImageLibrary} from 'react-native-image-picker'
+
+
 
 class AddDish extends React.Component {
 
     state = {
         isReady: false,
-        categoriesList: []
+        categoriesList: [],
+        dishImg: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw_D80Fz0Hez3_O4hI7QI4IeNDbIE21QDZkA&usqp=CAU',
+
+        category: '',
+        dishName: '',
+        weightDish: '',
+        consist: '',
+        priceDish: '',
+        descriptionDish: '',
+        imgData: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw_D80Fz0Hez3_O4hI7QI4IeNDbIE21QDZkA&usqp=CAU'
     }
 
     componentDidMount() {
@@ -25,11 +37,42 @@ class AddDish extends React.Component {
             console.log(error)
         }
     }
+    
+    handleChoosePhoto = () => {
+        const options = {
+            mediaType: 'photo'
+        }
+        launchImageLibrary(options, response => {
+            console.log('response', response)
+            const blob = response.blob()
+            this.setState({dishImg: response.uri, imgData: blob})
+            console.log(blob)
+        })
+    }
+
+    sendToServer = async() => {
+        console.log(this.state.descriptionDish)
+        try {
+            const res = await fetch(myURL+'/addDishToRestaurant?' + new URLSearchParams({
+                idCategory: this.state.category,
+                nameDish: this.state.dishName,
+                description: this.state.descriptionDish,
+                photoDish: this.state.imgData,
+                priceDish: this.state.priceDish,
+                idRest: Number(this.props.idRest.map((e) => e.RestaurantidRestaurant))
+            }))
+            const resText = res.json()
+            console.log(resText)
+        } catch(error) {
+            console.log(error);
+        }
+        
+    }
 
     shouldRender = () => {
         if (this.state.isReady) {
-            console.log(this.state.categoriesList)
             return (
+                <ScrollView>
                 <View style = {AddDishStyle.mainView}>
                     <View style = {{minHeight: 100}}>
                         <Text style = {AddDishStyle.defaultText}>Выберите категорию:</Text>
@@ -40,25 +83,67 @@ class AddDish extends React.Component {
                             dropDownStyle = {{width: 250, alignSelf: 'center', minHeight: 300}}
                             globalTextStyle = {{fontFamily: 'Montserrat-Light', fontSize: 16}}
                             itemStyle = {{borderBottomWidth: 1, borderColor: '#C1C1C1'}}
+                            onChangeItem = {(stat) => {this.setState({category: stat.value})}}
                         />
                     </View>
                     <View>
                         <Text style = {AddDishStyle.defaultText}>Название блюда:</Text>
                         <TextInput 
                             style = {AddDishStyle.inputs}
+                            onChangeText={(text) => this.setState({dishName: text})}
                         />
 
                         <Text style = {AddDishStyle.defaultText}>Вес блюда:</Text>
                         <TextInput 
                             style = {AddDishStyle.inputs}
+                            onChangeText={(text) => this.setState({weightDish: text})}
                         />
 
                         <Text style = {AddDishStyle.defaultText}>Состав блюда:</Text>
                         <TextInput 
                             style = {AddDishStyle.inputs}
+                            onChangeText={(text) => this.setState({consist: text})}
+                        />
+                        <Text style = {AddDishStyle.defaultText}>Фото блюда:</Text>
+                        <View style = {{flexDirection: 'row-reverse', justifyContent: 'space-between', marginBottom: 20}}>
+                            <TouchableOpacity
+                                onPress = {() => {
+                                    this.handleChoosePhoto()
+                                }}
+                                style = {AddDishStyle.choosePhoto}
+                            >
+                                <Text style = {AddDishStyle.defaultText}>Выбрать</Text>
+                            </TouchableOpacity>
+                            <Image 
+                                source = {{uri: this.state.dishImg}} 
+                                style = {{width: 150, height: 150}}
+                            />
+                        </View>
+
+                        <Text style = {AddDishStyle.defaultText}>Цена:</Text>
+                        <TextInput 
+                            style = {AddDishStyle.inputs}
+                            onChangeText={(text) => this.setState({priceDish: text})}
                         />
                         </View>
+
+                        <TouchableOpacity
+                            style = {AddDishStyle.buttonSend}
+                            onPress = {() => {
+                                if (this.state.dishName.length == 0) {
+                                    Alert.alert('Название блюда не может быть пустым')
+                                } else if (this.state.priceDish.length == 0) {
+                                    Alert.alert('Введите стоимость блюда')
+                                } else {
+                                    this.setState({descriptionDish: this.state.weightDish + '\r\n' + this.state.consist}, this.sendToServer)
+                                }
+                            }}
+                        >
+                            <Text style = {{fontFamily: 'Montserrat-Regular', fontSize: 22, color: 'white'}}>Добавить блюдо</Text>
+                        </TouchableOpacity>
+                        
                 </View>
+                </ScrollView>
             )
         } else {
             return (
@@ -72,7 +157,7 @@ class AddDish extends React.Component {
         }
     }
     render() {
-        console.log(Number(this.props.idRest.map((e) => e.RestaurantidRestaurant)))
+        //console.log(Number(this.props.idRest.map((e) => e.RestaurantidRestaurant)))
         
         return (
             <this.shouldRender/>
--- src/components/ManagerComponents/DishesEdit/AddDishStyle.js
+++ src/components/ManagerComponents/DishesEdit/AddDishStyle.js
@@ -16,6 +16,25 @@ const AddDishStyle = StyleSheet.create({
     mainView: {
         flex: 1,
         margin: 20
+    },
+    choosePhoto: {
+        justifyContent: 'center',
+        borderWidth: 2,
+        borderColor: "#FECA57",
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 20,
+        height: 40,
+        alignSelf: 'center'
+        
+    },
+    buttonSend: {
+        alignSelf: 'center',
+        marginVertical: 30,
+        borderRadius: 15,
+        paddingHorizontal: 70,
+        paddingVertical: 10,
+        backgroundColor: '#31B9CC'
     }
 })
 
